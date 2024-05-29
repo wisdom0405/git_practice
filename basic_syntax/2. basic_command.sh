0f9f8091cdfbcb824a25ed7177e0563e41b8c167
wisdom0405 <wisdom0405@kakao.com>
2024-05-29 14:48:40 +0900

    test commit
    1. 회원가입 2.회원목록조회 수정

--- basic_syntax/2. basic_command.sh
+++ basic_syntax/2. basic_command.sh
@@ -3,3 +3,14 @@ git status
 
 # commit 이력 확인 (local repository의 이력)
 git log
+
+# git add : 모든 수정/추가사항 add
+git add .
+
+# git add 특정파일 : 특정파일 add
+git add 특정파일(위치포함)
+
+# local repository에 반영 및 커밋이력 생성
+git commit -m "메시지 타이틀" -m "메시지 내용"
+
+# commit 이력 확인(local repository)
