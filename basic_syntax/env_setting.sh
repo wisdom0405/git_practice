dfafac3fed82d9ba0365c25d13f90b97e54501c7
wisdom0405 <wisdom0405@kakao.com>
2024-05-29 11:11:15 +0900

    fix

--- basic_syntax/env_setting.sh
+++ basic_syntax/env_setting.sh
@@ -1,3 +1,20 @@
+# git add .은 모든 변경사항 staging area로 이동
+git add .
+
+# git commit은 staging area의 변경사항을 확정짓고 commitID생성
+git commit -m "메시지"
+
+# 원격 repo에 upload
+git push origin 브랜치명
+
+# github 인증방법 2가지
+# 1.token 인증방식 : github에서 직접 보안키(token) 발급
+
+
+# 2.제3자 인증(oauth)방식
+
+
+
 # git 프로젝트 생성방법 2가지
 # 방법1. 원격 repo 생성 후 clone
 
